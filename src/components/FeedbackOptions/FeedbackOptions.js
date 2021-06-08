e9e17a8dfb3f4afe8b9d3cb9fc42a7ba3a63ea39
topsart <64708421+topsart@users.noreply.github.com>
2021-06-08 17:03:31 +0300

    init

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -2,13 +2,37 @@ import React from "react";
 // import styles from "./FeedbackOptions.module.css";
 
 class FeedbackOptions extends React.Component {
+  state = {
+    value: 5,
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      value: 6,
+    });
+  };
+  handleDecrement = () => {
+    this.setState({
+      value: 4,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
-      </div>
+      <>
+        <div>
+          <button onClick={this.handleIncrement}>Good</button>
+          <button onClick={this.handleIncrement}>Neutral</button>
+          <button onClick={this.handleIncrement}>Bad</button>
+        </div>
+
+        <div>
+          <h1>Statistics</h1>
+          <p>Good: {this.state.value}</p>
+          <p>Neutral: {this.state.value}</p>
+          <p>Bad: {this.state.value}</p>
+        </div>
+      </>
     );
   }
 }
